3969fb67b5b85406a3dee51c786e5b5e95ccda19
Indra <indra4837@gmail.com>
2020-05-26 02:12:53 +0800

    Main function working

--- historical_data/main.py
+++ historical_data/main.py
@@ -1,6 +1,11 @@
-from .candlestick_plot import plotly_candlestick_all_currency
-from .pull_historical_data import pull_data
+from candlestick_plot import plotly_candlestick_all_currency
+from pull_historical_data import pull_data
 
-if __name__ == "__main__":
+
+def main():
     pull_data()
     plotly_candlestick_all_currency()
+
+
+if __name__ == "__main__":
+    main()
