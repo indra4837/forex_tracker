c574208d729394b9b9b2bdb5cd4d7b7b5c97bfe3
Indra <indra4837@gmail.com>
2020-05-26 05:23:08 +0800

    Provides short or long

---
+++ Backtesting/backup_code.py
@@ -0,0 +1,22 @@
+# candle_data = candle_data['EUR/USD'].drop(columns=['Date'])
+    # s = (candle_data.iloc[0:1]).max()
+    # print(s.max())
+    # print(len(candle_data['EUR/USD']))
+    # for i in range(len())
+    # print(len(ema_data['EUR/USD'][0]['ema8']))
+    # for i in range(len(ema_data)):
+
+# get_extreme_value_ema(ema_data[k][0]['ema8'], ema_data[k][1]['ema13'], ema_data[k][2]['ema21'])
+
+
+# temp_ema8 = ema8.drop(columns='Date')
+# temp_ema13 = ema13.drop(columns='Date')
+# temp_ema21 = ema21.drop(columns='Date')
+
+# for i in range(len(temp_ema8)):
+#    ema8_value = temp_ema8.iloc[i]
+#    ema13_value = temp_ema13.iloc[i]
+#    ema21_value = temp_ema21.iloc[i]
+#    print(ema8_value.item())
+# print (ema8)
+# print(max(ema8, ema13))
