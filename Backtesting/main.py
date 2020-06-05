59ebf6b1e8786eff808acf83046d8ba3fdaeab60
Indra <indra4837@gmail.com>
2020-06-06 02:42:24 +0800

    Created main function to implement strategy

---
+++ Backtesting/main.py
@@ -0,0 +1,10 @@
+from Backtesting import trend_follow_strategy
+
+
+def main():
+    trend_follow_strategy.stoploss_takeprofit()
+    print("Trading setup calculated...")
+
+
+if __name__ == "__main__":
+    main()
